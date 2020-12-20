6f4d543a754c0e669655f858fbdadb7b663349d9
Andrew <vu035@cougars.csusm.edu>
2020-12-20 15:56:43 -0800

    updating styling on login page, navigation bar and create post modal's button

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,22 +1,25 @@
 import React, { useEffect } from 'react';
 import { Link } from "react-router-dom";
-import Text from './Text.js';
 import ProfilePicture from './ProfilePicture.js';
+import "../fonts/fonts.css"
 
 import home from "../images/icons/logo1accent.png";
 import trend from "../images/icons/trendingaccent-1.png";
 import follow from "../images/icons/followingaccent-1.png";
 import setting from "../images/icons/settingsaccent-1.png";
+import editGame from "../images/icons/editgameaccent-1.png"
 
 function NavigationBar(props) {
     const iconStyle = {
-        width: "50px",
-        height: "50px"
+        width: "40px",
+        height: "40px",
+        marginRight: ".5rem"
     };
 
     const linkStyle = {
         color: "#BF9AFC",
-        textDecoration: 'none'
+        textDecoration: 'none',
+        fontFamily: "SansationRegular"
     };
 
     useEffect(() => {
@@ -28,25 +31,26 @@ function NavigationBar(props) {
     return (
         <div className="NavigationBar" style={{ color: "#BF9AFC" }}>
             <Link to={"/profile/" + props.currentUserId} style={linkStyle}>
-            <ProfilePicture currentUserInfo={props.currentUserInfo} width="190px" height="190px" /> <p>Profile</p>
+                <ProfilePicture currentUserInfo={props.currentUserInfo} width="190px" height="190px" /> <p>Profile</p>
             </Link>
             <Link to="/" style={linkStyle}>
-                <p><img src={home} style={iconStyle} alt="" /> Home</p>
+                <p><img src={home} style={iconStyle} alt="" /> home</p>
             </Link>
             <Link to="/following" style={linkStyle}>
-                <p><img src={follow} style={iconStyle} alt="" /> Following</p>
+                <p><img src={follow} style={iconStyle} alt="" /> following</p>
             </Link>
             <Link to="/trending" style={linkStyle}>
-                <p><img src={trend} style={iconStyle} alt="" /> Trending</p>
+                <p><img src={trend} style={iconStyle} alt="" /> trending</p>
             </Link>
             <Link to="/settings" style={linkStyle}>
-                <p><img src={setting} style={iconStyle} alt="" /> Settings</p>
+                <p><img src={setting} style={iconStyle} alt="" /> settings</p>
             </Link>
-            <a id="editGamesToggle" data-toggle="modal" data-target="#chooseGamesModal" style={linkStyle} style={{cursor: "pointer"}}>
-                <p>Edit Games</p>
+            <a id="editGamesToggle" data-toggle="modal" data-target="#chooseGamesModal" style={{ cursor: "pointer" }} >
+                <p style={{ fontFamily: "SansationRegular" }}>
+                    <img src={editGame} style={iconStyle}></img>edit games</p>
             </a>
             <Link to="/" style={linkStyle}>
-                <p onClick={() => props.signOut()}>Logout</p>
+                <p onClick={() => props.signOut()}>logout</p>
             </Link>
         </div>
     );
