87d6ab288a3aa99694cc73ddc63bad88f70c71d9
Fawn <floopyrocks@gmail.com>
2020-12-24 00:00:11 -0500

    fixed login and sign up boxes

--- src/pages/CreateProfile.js
+++ src/pages/CreateProfile.js
@@ -26,9 +26,9 @@ function CreateProfile(props) {
   return (
     <div className="CreateProfile">
       <div className="mx-auto card text-center" style={{
-        margin: "40px",
+        margin: "150px",
         maxWidth: "485px",
-        backgroundColor: "#292823",
+        backgroundColor: "#292833",
         borderRadius: "10px",
         boxShadow: "-1px 10px 250px 1px #171421",
         color: "#BF9AFC"
@@ -37,7 +37,7 @@ function CreateProfile(props) {
         <h4>create your profile</h4>
         < br />
         Pick a username
-        <Input type="displayName" placeholder="username" track={setDisplayName} />
+        <Input type="displayName" placeholder="username" track={setDisplayName} style= {{ backgroundColor: "#292833" }}  />
         < br />
         add a profile picture
         <DisplayImage type="profileImage" id="createAvatar" currentImg={add} setImg={setImg} />
