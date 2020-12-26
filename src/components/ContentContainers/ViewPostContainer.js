f8cf1976e380e0feafeb775b7c650615d51dc0e1
CarrotWiz <carrotwiz22@gmail.com>
2020-12-26 15:08:39 -0500

    category tag on view post page

--- src/components/ContentContainers/ViewPostContainer.js
+++ src/components/ContentContainers/ViewPostContainer.js
@@ -48,7 +48,7 @@ function ViewPostContainer(props) {
 
     return (
         <div className="ViewPostContainer">
-            <Post {...props} post={post} postId={postId} showCommentButton={true}
+            <Post {...props} post={post} postId={postId} showCommentButton={true} showCategory={true}
                 style={{
                     paddingBottom: '0px',
                     paddingLeft: '0px',
