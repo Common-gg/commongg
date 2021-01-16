24c53223f72dc32060f841dd91e8edb8a67eb34b
Andrew <vu035@cougars.csusm.edu>
2021-01-15 20:17:50 -0800

    adding x button to modal, removing back button

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -124,45 +124,18 @@ function SignUp(props) {
     margin: "auto",
     border: "none"
   }
-  const modalFooterStyle = {
-    position: "flex",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    border: "none"
-  }
-  const history = useHistory();
-  const imageBackButtonStyle = {
-    width: "40px",
-    height: "45px",
-    paddingRight: ".5rem"
-  };
-  const backButtonStyle = {
-    backgroundColor: "transparent",
-    color: "#BF9AFC",
-    borderWidth: "2px",
-    padding: "0.6rem",
-    paddingTop: "4rem",
-    paddingLeft: "8rem"
-  };
 
   return (
-    <div>
-      <Modal show={showTosModal} style={modalStyle} backdrop="static" keyboard={false}>
+    <div className="SignUp">
+      <Modal show={showTosModal} style={modalStyle} >
         <Modal.Body style={modalBodyStyle}>
           <div className="row">
-            <button type="button"
-              className="btn"
-              style={backButtonStyle}
-              onClick={() => { history.goBack(); setShowTosModal(false); }}
-            >
-              <div>
-                <p style={{ fontSize: "25px" }}><img src={ArrowLeft} style={imageBackButtonStyle}></img>back</p>
-              </div>
+            <button type="button" style={{ marginLeft: "42rem", color: "#BF9AFC" }} className="close" aria-label="Close">
+              <span id="createPostX" aria-hidden="true" onClick={() => { setShowTosModal(false) }}>&times;</span>
             </button>
+            <TermsOfService />
+            <hr style={{ backgroundColor: '#BF9AFC', width: '90%', left: "5px" }} />
           </div>
-          <TermsOfService />
-          <hr style={{ backgroundColor: '#BF9AFC', width: '90%', left: "5px" }} />
         </Modal.Body>
 
       </Modal>
