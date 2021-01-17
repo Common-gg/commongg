ca66895d3e35916a1a96e3189898a8aac35a5606
CarrotWiz <carrotwiz22@gmail.com>
2021-01-17 02:02:55 -0500

    crop profile picture

---
+++ src/components/CropProfilePictureModal.js
@@ -0,0 +1,136 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { Modal } from "react-bootstrap";
+import ReactCrop from 'react-image-crop';
+import 'react-image-crop/dist/ReactCrop.css';
+
+function CropProfilePictureModal(props) {
+
+    const [show, setShow] = useState(false);
+    const [croppedImage, setCroppedImage] = useState({ src: null, crop: { x: 10, y: 10, width: 80, height: 80, aspect: 1 }, image: null });
+
+    useEffect(() => {
+        if (props.show === true) {
+            setShow(true);
+        } else {
+            setShow(false);
+        }
+    }, [props.show])
+
+    useEffect(() => {
+        setCroppedImage(props.croppedImage);
+    }, [props.croppedImage])
+
+    useEffect(() => {
+        //console.log(croppedImage);
+    }, [croppedImage])
+
+    let buttonStyle = {
+        backgroundColor: "transparent",
+        position: "relative",
+        left: "-.74rem"
+    }
+    const modalContentStyle = {
+        color: "#BF9AFC",
+        backgroundColor: "#202020",
+        borderBottom: "",
+    }
+    const modalHeaderStyle = {
+        borderBottom: "0 none",
+        textAlign: "center"
+    }
+
+    const clearFields = () => {
+        setCroppedImage({ src: null, crop: { x: 10, y: 10, width: 80, height: 80, aspect: 1 } });
+        props.setShowCropModal(false);
+    }
+
+    const getCroppedImg = (image, crop, fileName) => {
+
+        const canvas = document.createElement('canvas');
+        const scaleX = image.naturalWidth / document.getElementsByClassName("ReactCrop__image")[0].clientWidth; //image size from modal
+        const scaleY = image.naturalHeight / document.getElementsByClassName("ReactCrop__image")[0].clientHeight; //464 x 416
+        canvas.width = Math.ceil(crop.width * scaleX);
+        canvas.height = Math.ceil(crop.height * scaleY);
+        const ctx = canvas.getContext('2d');
+
+        ctx.drawImage(
+            image,
+            crop.x * scaleX,
+            crop.y * scaleY,
+            crop.width * scaleX,
+            crop.height * scaleY,
+            0,
+            0,
+            crop.width * scaleX,
+            crop.height * scaleY
+        );
+
+        //const base64Image = canvas.toDataURL('image/png')
+        //console.log(base64Image);
+        //props.setImage(base64Image);
+
+        canvas.toBlob(blob => {
+            blob.name = fileName;
+            props.setCropBlob(blob);
+        }, 'image/png', 1);
+    }
+
+    const handleCropClick = () => { //return img file
+        getCroppedImg(document.getElementById("imageId"), croppedImage.crop, "profile_picture.png");
+        props.setShowCropModal(false);
+    }
+
+    const onImageLoaded = (image) => {
+        //console.log("onCropComplete", image);
+
+    };
+
+    const onCropComplete = (crop) => {
+        //console.log("onCropComplete", crop);
+    };
+
+    const onCropChange = (crop) => {
+        setCroppedImage({ ...croppedImage, crop });
+    };
+
+    const modalBody = (croppedImage) => {
+        if (croppedImage.src !== undefined) {
+            return (
+                <Modal.Body className="text-center" >
+                    {croppedImage.src && (
+                        <ReactCrop
+                            src={croppedImage.src}
+                            crop={croppedImage.crop}
+                            onImageLoaded={onImageLoaded}
+                            onComplete={onCropComplete}
+                            onChange={onCropChange}
+                            circularCrop={true}
+                        />
+                    )}
+                </Modal.Body>
+            )
+        }
+    }
+
+    return (
+        <div className="CropProfilePictureModal">
+            <img src={croppedImage.src} id="imageId" style={{ display: "none" }} />
+            <Modal show={show} onHide={() => props.setShowCropModal(false)} dialogClassName="modal-100w" centered scrollable>
+                <div className="modal-content" style={modalContentStyle}>
+                    <div className="modal-header" style={modalHeaderStyle}>
+                        <h5 className="modal-title" id="cropProfilePictureModalLabel">Crop Profile Picture</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={() => clearFields()}>
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    {modalBody(croppedImage)}
+                    <div style={{ display: "flex" }}>
+                        <button type="button" className="btn btn-primary" onClick={() => handleCropClick()} data-dismiss="modal">Crop</button>
+                    </div>
+                </div>
+            </Modal>
+        </div>
+    )
+}
+
+export default CropProfilePictureModal;
--- src/components/DisplayImage.js
+++ src/components/DisplayImage.js
@@ -1,8 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Compress from "compress.js";
+import CropProfilePictureModal from "./CropProfilePictureModal.js";
 
 function DisplayImage(props) {
   const [image, setImage] = useState(props.currentImg);
+  const [showCropModal, setShowCropModal] = useState(false);
+  const [croppedImage, setCroppedImage] = useState({ src: null, crop: { x: 10, y: 10, width: 80, height: 80, aspect: 1 } });
+  const [cropBlob, setCropBlob] = useState();
+
+  useEffect(() => {
+    if (cropBlob === undefined) return;
+    console.log(cropBlob);
+    setImage(URL.createObjectURL(cropBlob));
+    props.setImg(cropBlob);
+  }, [cropBlob])
+
+  const onSelectFile = (event) => {
+    if (event.target.files && event.target.files[0]) {
+      const reader = new FileReader();
+      reader.addEventListener(
+        "load",
+        () => (
+          setCroppedImage({
+            ...croppedImage,
+            src: reader.result
+          })
+        ),
+        false
+      );
+      reader.readAsDataURL(event.target.files[0]);
+      setShowCropModal(true);
+    }
+  }
 
   const onImageChange = event => {
     if (event.target.files && event.target.files[0]) {
@@ -54,7 +83,9 @@ function DisplayImage(props) {
             name="UserImage"
             accept="image/*"
             style={{ display: "none" }}
-            onChange={onImageChange} />
+            onChange={onSelectFile}
+          />
+          <CropProfilePictureModal show={showCropModal} setShowCropModal={setShowCropModal} croppedImage={croppedImage} setImage={setImage} setCropBlob={setCropBlob} />
         </div>
       </div>
     </div>
