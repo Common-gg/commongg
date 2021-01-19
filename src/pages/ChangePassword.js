b521dcbfc51d0a9d1ec959df4903ab514fbfbd4f
Andrew <vu035@cougars.csusm.edu>
2021-01-18 19:30:50 -0800

    adding confirm password input, applicable validation, enforcing password strength on confirm password page to change password

--- src/pages/ChangePassword.js
+++ src/pages/ChangePassword.js
@@ -4,9 +4,12 @@ import logo from "../images/icons/logo1light.png";
 import { Link } from "react-router-dom"
 
 function ChangePassword(props) {
-
-    const [password, setPassword] = useState();
+    const initialInputState = { current: { value: "" } };
+    const [password, setPassword] = useState(initialInputState);
+    const [confirmPassword, setConfirmPassword] = useState(initialInputState);
     const [oobCode, setOobCode] = useState();
+    const [displayValidationForWeakPassword, setDisplayValidationForWeakPassword] = useState(false);
+    const [isConfirmPasswordValidationDisplayed, setIsConfirmPasswordValidationDisplayed] = useState(false);
     const [isPasswordResetSuccess, setIsPasswordResetSuccess] = useState(null);
 
     useEffect(() => {
@@ -14,6 +17,20 @@ function ChangePassword(props) {
         setOobCode(url.searchParams.get("oobCode"));
     }, [])
 
+    const inputStyle = {
+        backgroundColor: "transparent #292833",
+        color: "#BF9AFC",
+        border: "solid",
+        borderColor: "#BF9AFC",
+        borderWidth: "1px",
+        borderRadius: "2px",
+        backgroundColor: "#292833",
+        padding: "0.3rem",
+        margin: "3%",
+        width: "80%",
+        height: "77%",
+        marginLeft: "10%"
+    }
     const logoCSS = {
         /* logo1light 1 */
         width: "100px",
@@ -21,7 +38,6 @@ function ChangePassword(props) {
         left: "393px",
         top: "179px"
     }
-
     const buttonStyle = {
         backgroundColor: "transparent",
         color: "#BF9AFC",
@@ -31,21 +47,54 @@ function ChangePassword(props) {
         borderWidth: "2px"
     }
 
+    function clearValidationBools() {
+        setIsPasswordResetSuccess(null);
+        setIsConfirmPasswordValidationDisplayed(false);
+        setDisplayValidationForWeakPassword(false);
+    }
+
     function handleChangePasswordClick() {
-        props.handleResetPassword(oobCode, password.current.value, setIsPasswordResetSuccess);
-        password.current.value = "";
+        clearValidationBools();
+
+        const validatePasswordRegex = /^(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){1,})(?=(.*[!@#$%^&*()\-__+.]){1,}).{6,}$/
+        let passwordStrength = password.current.value.match(validatePasswordRegex);
+
+        if (passwordStrength === null) {
+            setDisplayValidationForWeakPassword(true);
+            return;
+        }
+        else if (password.current.value === "" || confirmPassword.current.value === "") {
+            return;
+        }
+        else if (password.current.value !== confirmPassword.current.value) {
+            setIsConfirmPasswordValidationDisplayed(true);
+            return;
+        }
+        else {
+            props.handleResetPassword(oobCode, password.current.value, setIsPasswordResetSuccess);
+            password.current.value = "";
+            confirmPassword.current.value = "";
+        }
         handleMessagingForChangePassword();
     }
 
     function handleMessagingForChangePassword() {
-        if (isPasswordResetSuccess === null) {
-            return (<div></div>);
+        if (isPasswordResetSuccess === true) {
+            return (<p style={{ color: "green" }}>password reset successful! return to login page to continue</p>);
+        }
+        else if (displayValidationForWeakPassword === true) {
+            return (<p style={{ color: "#F34D4D" }}>passwords must have at least 6 characters, 1 uppercase letter, 1 lowercase letter, 1 number</p>);
         }
         else if (isPasswordResetSuccess === false) {
             return (<p style={{ color: "#F34D4D" }}>there was an error when trying to reset your password</p>);
         }
+        else if (isConfirmPasswordValidationDisplayed === true) {
+            return (<p style={{ color: "#F34D4D" }}>Password field and confirm password field do not match. Ensure they match and try again.</p>);
+        }
         else {
-            return (<p style={{ color: "green" }}>password reset successful! return to login page to continue</p>);
+            return (
+                <div></div>
+            );
         }
     }
 
@@ -65,47 +114,37 @@ function ChangePassword(props) {
                         <img src={logo} style={logoCSS} />
                         <div className="col-5" style={{ pointerEvents: "none" }}></div>
                         <br />
-                        <div className="form-group" >
-                            <div className="row">
-                                <div className="col-6"></div>
-                                <div className="row">
-                                    <div className="col-6"></div>
-                                    <div className="row">
-                                        <div className="col-6"></div>
-                                        <div className="row">
-                                            <div className="col-4"></div>
-                                            <br />
-                                            <Input type="password"
-                                                bootstrap="border border-secondary"
-                                                placeholder="password"
-                                                style={{ backgroundColor: "#292833", }}
-                                                track={setPassword} />
-                                        </div></div></div></div></div></div>
-                    <div className="form-group">
-                        <div className="row col-12">
-                            <div className="col-1"></div>
-                            <div className="row">
-                                <div className="col-1"></div>
-                                <div className="row">
-                                    <div className="col-1"></div>
-                                    <div className="row">
-                                        <div className="col-1"></div>
-                                        <div className="row">
-                                            <div className="col-1"></div>
-                                        </div></div></div></div></div></div>
+                        <div className="form-group col-12">
+                            <div className="row mx-auto">
+                                <Input type="password"
+                                    bootstrap="border-0"
+                                    placeholder="new password"
+                                    style={{ backgroundColor: "#292833", }}
+                                    track={setPassword}
+                                    style={inputStyle}
+                                />
+                            </div>
+                        </div>
+                        <div className="form-group col-12">
+                            <div className="row mx-auto">
+                                <Input type="password"
+                                    bootstrap="border-0"
+                                    placeholder="confirm new password"
+                                    style={{ backgroundColor: "#292833" }}
+                                    track={setConfirmPassword}
+                                    style={inputStyle}
+                                />
+                            </div>
+                        </div>
+                    </div>
                     <div className="d-flex justify-content-center">
                         {handleMessagingForChangePassword()}
                     </div>
-                    <div className="row col-12" style={{
-                        justifyContent: "center",
-                        alignItems: "center"
-                    }}>
-                        <div className="form-group">
-                            <button className="btn btn-info" onClick={handleChangePasswordClick}
-                                style={buttonStyle} >
-                                change password
-                            </button>
-                        </div>
+                    <div className="d-flex justify-content-center">
+                        <button className="btn btn-info" onClick={handleChangePasswordClick}
+                            style={buttonStyle} >
+                            change password
+                        </button>
                     </div>
                     <div className="row">
                         <div className="col-5"></div>
