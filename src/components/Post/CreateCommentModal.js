484617a4359d78970fbbd7b282bbfd526b67ebe1
Andrew <vu035@cougars.csusm.edu>
2021-01-19 18:29:23 -0800

    handle clicking outside comment modal

--- src/components/Post/CreateCommentModal.js
+++ src/components/Post/CreateCommentModal.js
@@ -5,8 +5,10 @@ import { Modal } from "react-bootstrap";
 function CreateCommentModal(props) {
     const [show, setShow] = useState(false);
     const commentTextRef = useRef();
+    const modalRef = useRef(null);
     const [commentText, setCommentText] = useState({ current: { value: "" } });
     const [displayCommentTextLengthValidationMessage, setDisplayCommentTextLengthValidationMessage] = useState(false);
+    const [showClickOutsideCommentModal, setShowClickOutsideCommentModal] = useState(false);
 
     useEffect(() => {
         //if parent want the modal to show directly do it
@@ -14,8 +16,6 @@ function CreateCommentModal(props) {
             setShow(true);
         };
     }, [props.show])
-
-    const handleClose = () => setShow(false);
 
     let buttonStyle = {
         backgroundColor: "transparent",
@@ -32,7 +32,7 @@ function CreateCommentModal(props) {
     }
     const modalHeaderStyle = {
         borderBottom: "0 none",
-        textAlign: "center", 
+        textAlign: "center",
         paddingLeft: "5%",
     }
     const textAreaStyle = {
@@ -45,12 +45,12 @@ function CreateCommentModal(props) {
         borderBottom: "0",
         whiteSpace: "pre-wrap",
         paddingLeft: "5%",
-        paddingRight: "5%", 
+        paddingRight: "5%",
         overflow: "wrap"
     }
-    const commentButtonStyle = {
+    const modalButtonStyle = {
         height: 48,
-        margin: "1%" ,
+        margin: "1%",
         marginLeft: "auto",
         marginRight: "5%",
         backgroundColor: "#BF9AFC",
@@ -59,6 +59,16 @@ function CreateCommentModal(props) {
         borderRadius: "10px",
         borderColor: "#BF9AFC",
         borderWidth: "1px",
+    }
+    const clickOutsideModalContentStyle = {
+        color: "#BF9AFC",
+        backgroundColor: "#292833",
+        borderTop: "0",
+        borderLeft: "0",
+        borderRight: "0",
+        borderBottom: "0",
+        width: "600px",
+        height: "150px",
     }
 
     function clearFields() {
@@ -80,6 +90,8 @@ function CreateCommentModal(props) {
         createComment();
     }
 
+    const handleClose = () => setShow(false);
+
     function createComment() {
         props.createComment({
             author: props.currentUserId,
@@ -89,6 +101,16 @@ function CreateCommentModal(props) {
         }, props.post.author);
         props.updateRefresh();
         clearFields();
+    }
+
+    function handleMouseDown(e) {
+        if (modalRef.current && !modalRef.current.contains(e.target)) {
+            setShowClickOutsideCommentModal(true);
+        }
+    }
+    function handleYesClick() {
+        setShowClickOutsideCommentModal(false);
+        setShow(false);
     }
 
     return (
@@ -101,35 +123,55 @@ function CreateCommentModal(props) {
                         height: "1.625rem"
                     }} />
             </button>
-            <Modal show={show} onHide={handleClose} onEntered={() => commentTextRef.current.focus()}>
-                <div className="modal-content" style={modalContentStyle}>
-                    {displayCommentTextLengthValidationMessage ?
-                        <p style={{ color: "#F34D4D" }}>
-                            The length of your comment cannot exceed 1000 characters
-                        </p> : null}
+            <Modal show={showClickOutsideCommentModal} backdrop="static" style={{ marginRight: "5rem", marginTop: "15rem", zIndex: "99999" }
+            } onHide={() => { setShowClickOutsideCommentModal(false) }}>
+                <div className="modal-content" style={clickOutsideModalContentStyle}>
                     <div className="modal-header" style={modalHeaderStyle}>
-                        <h5 className="modal-title" id="createCommentModalLabel">create a comment</h5>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close"  style={{ color: "#BF9AFC" }} onClick={() => clearFields()}>
-                            <span aria-hidden="true" style={{ color: "#BF9AFC" }}>&times;</span>
-                        </button>
+                        <h5 className="modal-title" >Are you sure you want to click outside comment?</h5>
                     </div>
-                    <hr style={{ padding: "0", backgroundColor: '#5F5177', width: '90%' }} />
-                    <textarea
-                        className="form-control"
-                        onChange={() => setCommentText(commentTextRef)}
-                        ref={commentTextRef}
-                        placeholder="type your comment here..."
-                        rows="5"
-                        style={textAreaStyle}
-                        maxLength="1000"
-                    />
-                    <hr style={{ padding: "0", backgroundColor: '#5F5177', width: '90%' }} />
-                    <div style={{ display: "flex" }}>
-                        <button type="button" className="btn btn-primary" onClick={() => handleCommentClick()} data-dismiss="modal" style={commentButtonStyle}>Comment</button>
+                    <div>
+                        <hr style={{ padding: "0", backgroundColor: '#5F5177', width: '90%' }} />
+                        <div style={{ display: "flex", justify: "center" }}>
+                            <button type="button" className="btn btn-primary" onClick={() => setShowClickOutsideCommentModal(false)}
+                                style={{
+                                    ...modalButtonStyle, marginLeft: "23.5rem"
+                                }}>no, i'll stay</button>
+                            <button type="button" className="btn btn-primary" onClick={handleYesClick} style={modalButtonStyle}>yes</button>
+                        </div>
                     </div>
-                    <br />
                 </div>
             </Modal>
+            <div onMouseDown={handleMouseDown} >
+                <Modal show={show} onHide={handleClose} backdrop={showClickOutsideCommentModal ? "static" : "dynamic"} style={{ zIndex: "99998" }} onEntered={() => commentTextRef.current.focus()}>
+                    <div className="modal-content" ref={modalRef} style={modalContentStyle}>
+                        {displayCommentTextLengthValidationMessage ?
+                            <p style={{ color: "#F34D4D" }}>
+                                The length of your comment cannot exceed 1000 characters
+                        </p> : null}
+                        <div className="modal-header" style={modalHeaderStyle}>
+                            <h5 className="modal-title" id="createCommentModalLabel">create a comment</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close" onClick={() => clearFields()}>
+                                <span aria-hidden="true" style={{ color: "#BF9AFC" }}>&times;</span>
+                            </button>
+                        </div>
+                        <hr style={{ padding: "0", backgroundColor: '#5F5177', width: '90%' }} />
+                        <textarea
+                            className="form-control"
+                            onChange={() => setCommentText(commentTextRef)}
+                            ref={commentTextRef}
+                            placeholder="type your comment here..."
+                            rows="5"
+                            style={textAreaStyle}
+                            maxLength="1000"
+                        />
+                        <hr style={{ padding: "0", backgroundColor: '#5F5177', width: '90%' }} />
+                        <div style={{ display: "flex" }}>
+                            <button type="button" className="btn btn-primary" onClick={() => handleCommentClick()} data-dismiss="modal" style={modalButtonStyle}>Comment</button>
+                        </div>
+                        <br />
+                    </div>
+                </Modal>
+            </div>
         </div>
     )
 }
