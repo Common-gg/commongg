49d7dee23ac1ae40df2c16f47f252b2b74eec08d
David Stewart <darkorin00@hotmail.com>
2021-01-28 16:05:31 -0500

    fixed errors from the merges

--- src/components/ReactionIcon.js
+++ src/components/ReactionIcon.js
@@ -16,6 +16,7 @@ function ReactionIcon(props) {
       });
     }
     if (props.usersReacted) {
+      console.log(props.usersReacted(props.reaction));
       if (props.usersReacted.length === 1) {
         const toolTipString = `${props.usersReacted[0]} reacted with ${props.reaction}`;
         setToolTipText(toolTipString);
