271957cdf7fea68d27a89b156940ed523e4fa214
Sean Yang <seanyang0813@gmail.com>
2021-02-03 00:51:38 -0500

    fix and add cur users reacted

--- src/components/ReactionIcon.js
+++ src/components/ReactionIcon.js
@@ -17,6 +17,7 @@ function ReactionIcon(props) {
       });
     }
     if (props.usersReacted) {
+      const curUsersReacted = props.usersReacted(props.reaction)
       if (props.usersReacted.length === 1) {
         const toolTipString = `${props.usersReacted[0]} reacted with ${props.reaction}`;
         setToolTipText(toolTipString);
